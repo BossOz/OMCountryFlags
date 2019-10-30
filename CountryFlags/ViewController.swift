9736c72b82bbbc65720734b64c308c8c1954317c
Oscar Manzelli <omanzelli@gmail.com>
2019-10-30 20:03:07 +0100

    Initial Commit

---
+++ CountryFlags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryFlags
+//
+//  Created by Oscar on 30/10/2019.
+//  Copyright © 2019 Oscar Manzelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
